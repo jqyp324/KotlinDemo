aa1c49c99eb83be782d3d567417d38c65e69bedd
jqyp324 <jqyp324@gmail.com>
2018-08-08 00:03:48 +0800

    1

--- src/first/MFirst.kt
+++ src/first/MFirst.kt
@@ -20,9 +20,35 @@ fun main(args: Array<String>) {
     println(ff)
     f += 4
     println(f + 4)
+    println(ifHanshu(5,6))
+    println("the value is ${ifNull("1")}")
+    println("the value is ${ifNull("a")}")
+    println("the 1 is type of ${isString(1)}")
+
+
+    val mylist = listOf("1","2","3")
+    println(mylist.javaClass)
+
+    for (item in mylist){
+        println(item)
+    }
+
+    for (index in mylist.indices){
+        println("$index is ${mylist[index]}")
+    }
+
 }
 
 fun sum(a: Int, b: Int) = a + b
+
+fun ifHanshu(a:Int,b:Int) = if (a > b) a else b
+
+fun ifNull(str:String):Int?{
+    return str.toIntOrNull()
+}
+
+fun isString(str: Any) = str is String
+
 
 fun myPrint() {
     println("4 + 5 = ${sum(4, 5)}")
