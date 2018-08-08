786bf8a0c857fa4dd4b9629fa663063372881005
jqyp324 <jqyp324@gmail.com>
2018-08-09 00:16:07 +0800

    ready started

--- src/first/MFirst.kt
+++ src/first/MFirst.kt
@@ -20,30 +20,54 @@ fun main(args: Array<String>) {
     println(ff)
     f += 4
     println(f + 4)
-    println(ifHanshu(5,6))
+    println(ifHanshu(5, 6))
     println("the value is ${ifNull("1")}")
     println("the value is ${ifNull("a")}")
     println("the 1 is type of ${isString(1)}")
 
 
-    val mylist = listOf("1","2","3")
+    val mylist = listOf("1", "2", "3")
     println(mylist.javaClass)
 
-    for (item in mylist){
+    for (item in mylist) {
         println(item)
     }
 
-    for (index in mylist.indices){
+    for (index in mylist.indices) {
         println("$index is ${mylist[index]}")
+    }
+
+    println("mylist indices->${mylist.indices}")
+
+    println(descript(1))
+
+    val x = 10
+    val y = 9
+    if (x in 1..y+1)
+        println("$x is in 1-${y+1}")
+
+    for (x in 1..5)
+        println("index -> $x")
+
+    for (x in 1..5 step 2)
+        println("index step -> $x")
+
+    val mySet = setOf("a","b","c","a")
+    println(mySet.javaClass)
+    println(mySet)
+    when{
+//        "a" in mySet -> println("a is in my set")
+        "c" in mySet -> println("c is in my set")
+        "d" in mySet -> println("d is not in my set")
     }
 
 }
 
 fun sum(a: Int, b: Int) = a + b
 
-fun ifHanshu(a:Int,b:Int) = if (a > b) a else b
+fun ifHanshu(a: Int, b: Int) = if (a > b) a else b
 
-fun ifNull(str:String):Int?{
+fun ifNull(str: String): Int? {
     return str.toIntOrNull()
 }
 
@@ -52,4 +76,12 @@ fun isString(str: Any) = str is String
 
 fun myPrint() {
     println("4 + 5 = ${sum(4, 5)}")
+}
+
+fun descript(obj: Any) = when (obj) {
+    1 -> "One"
+    "Hello" -> "String"
+    is String -> "this is String"
+    else -> "Unknow"
+
 }
