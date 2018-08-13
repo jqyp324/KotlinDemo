d397f9c67c481c7e37280a84b6e0dad7fcc1fb98
yupeng112527 <yupeng112527@pwrd.com>
2018-08-13 19:30:25 +0800

    代理对象和属性代理

---
+++ src/first/MProxy.kt
@@ -0,0 +1,50 @@
+package first
+
+import kotlin.reflect.KProperty
+
+fun main(args: Array<String>) {
+    println("===代理===")
+
+    val base = ProxyBaseImpl()
+    Derived(base).print()
+
+    val myExample = Example()
+    myExample.myvar = "yupeng"
+    println(myExample.myvar)
+
+}
+
+interface ProxyBase{
+    fun print()
+}
+
+class ProxyBaseImpl:ProxyBase{
+    override fun print() {
+        println("ProxyBaseImpl")
+    }
+}
+
+class Derived(d:ProxyBase):ProxyBase by d
+
+class Example{
+    var myvar :String by MyDelegate()
+}
+
+class MyDelegate{
+    operator fun getValue(example: Example, property: KProperty<*>): String {
+        return "$example ===delegate===${property.name}"
+    }
+
+    operator fun setValue(example: Example, property: KProperty<*>, s: String) {
+        println("set")
+
+    }
+//    operator fun getValue(thisRef: Any?, property: KProperty<*>): String {
+//        return "$thisRef, thank you for delegating '${property.name}' to me!"
+//    }
+//    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: String) {
+//        println("$value has been assigned to '${property.name} in $thisRef.'")
+//    }
+
+
+}
