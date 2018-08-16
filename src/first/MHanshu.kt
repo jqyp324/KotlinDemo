1779b6cbbabbc34884928a32fe75fe4f98f45b84
jqyp324 <jqyp324@gmail.com>
2018-08-16 23:28:26 +0800

    函数

--- src/first/MHanshu.kt
+++ src/first/MHanshu.kt
@@ -11,6 +11,12 @@ fun main(args: Array<String>) {
     println(1 shl 2) //用中缀注解调用扩展函数
 
     println(addNum(3))
+
+    val ll = intArrayOf(1,2,3,4)
+
+    println("可变参数的个数为:" + kebian(1,2,3,4,*ll))
+
+    println(findFixPoint(0.3))
 }
 
 infix fun Int.sh1(x:Int):Int{
@@ -20,3 +26,10 @@ infix fun Int.sh1(x:Int):Int{
 fun addNum(x:Int,y:Int = 3):Int{
     return x + y
 }
+
+fun kebian(vararg a:Int):Int{
+    return a.size
+}
+
+tailrec fun findFixPoint(x: Double = 1.0): Double
+        = if (x == Math.cos(x)) x else findFixPoint(Math.cos(x))
